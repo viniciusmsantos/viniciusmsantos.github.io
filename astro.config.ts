b59a0784209a0e1afe6cfc67253a87f02244d34e
Vinicius Martins <viniciusmarsan@gmai.com>
2026-07-04 00:52:52 -0300

    base

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,6 @@ import { expressiveCodeOptions, siteConfig } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.url,
-	base: '/vinicius-blog',
 	image: {
 		domains: ["webmention.io"],
 	},
